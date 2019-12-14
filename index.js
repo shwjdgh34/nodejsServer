70e9195a47f645cd5fd5d9776cad37c5f3c27a54
Jeongho Noh <nossi8128@gmail.com>
2019-12-14 11:55:11 +0900

    implemented route using just basic code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("hello");
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('this is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('this is the PRODUCT');
+    }
 })
 server.listen(8000, '127.0.0.1', () => {
     console.log('listening to requests on port 8000');
