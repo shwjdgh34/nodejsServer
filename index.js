ca44d0a702d110bbab524ef52d680ad6cc2f65db
Jeongho Noh <nossi8128@gmail.com>
2019-12-14 16:34:12 +0900

    modulation replaceTemplate function

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-
-const replaceTemplate = (tmplt, product) => {
-    let output = tmplt.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%NUTRIENT%}/g, product.nutrients);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%NOT_ORGANIC%}/g, product.organic ? '' : 'not-organic');
-    return output;
-};
+const replaceTemplate = require('./modules/replaceTemplate.js');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);   // JSON.parse method transform string to objec
