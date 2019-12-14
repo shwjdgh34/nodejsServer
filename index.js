5c1232afb7389a35c4a30360c0def5aa48f32e78
Jeongho Noh <nossi8128@gmail.com>
2019-12-14 13:40:42 +0900

    implemented simple API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
@@ -6,6 +7,16 @@ const server = http.createServer((req, res) => {
         res.end('this is the OVERVIEW');
     } else if (pathName === '/product') {
         res.end('this is the PRODUCT');
+    } else if (pathName === '/api') {
+        fs.readFile(`${__dirname}/dev-data/data.json`, (err, data) => {
+            const productData = JSON.parse(data);   // JSON.parse method transform string to object
+            res.writeHeader(200, { 'Content-Type': 'application/json' });
+            res.end(data);  // end method needs to send back a string not an object. 
+            if (err) {
+                throw err;
+            }
+        })
+
     } else {
         res.writeHead(404, {
             'Content-Type': 'text/html',
